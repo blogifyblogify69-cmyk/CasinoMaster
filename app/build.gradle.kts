1e3ff659b5ddd960125167d3d07ce6571f0244b1
blogifyblogify69-cmyk <blogifyblogify69@gmail.com>
2026-09-25 21:47:33 +0530

    Bump install-safe Android build to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -12,8 +11,8 @@ android {
         applicationId = "com.casinomaster"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.3.0"
     }
 
     compileOptions {
