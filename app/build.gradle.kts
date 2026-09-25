f332afd606a70671821aff606e1e34ed92f5c6b2
blogifyblogify69-cmyk <blogifyblogify69@gmail.com>
2026-09-25 21:39:20 +0530

    Bump app version for install-ready test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -11,8 +12,8 @@ android {
         applicationId = "com.casinomaster"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
 
     compileOptions {
